6f2e449ee5b9517353bf23304c90cf0bfa14ebd3
Mohammad Sohail <msohailcs@outlook.com>
2022-03-17 19:40:22 +0000

    Back end and front end complete, login, registration, logout fully functional

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,68 +1,110 @@
-import { useState, useEffect } from "react";
-import { FaSignInAlt} from "react-icons/fa";
+import {useState, useEffect} from 'react'
+import { FaSignInAlt } from "react-icons/fa"
+import {useSelector, useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {toast} from 'react-toastify'
+import { login, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Login() {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
+    const [formData, setFromData] = useState({
+        email: '',
+        password: '',
+    })
 
-  const { email, password } = formData;
+    const { email, password} = formData
 
-  //setFormData is one object, don't have to do for each setName, setEmail, setPassword...
-  const onChange = (e) => {
-      setFormData((prevState) => ({
-          ...prevState,  
-          [e.target.name]: e.target.value
-      }))
-  };
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
 
-  const onSubmit = (e) => {
-      e.preventDefault()
-  }
+    const {user, isLoading, isError, isSuccess, message} = useSelector(
+        (state) => state.auth 
+    )
 
-  return (
-    <>
-      <section className="heading">
+    useEffect(() => {
+      if(isError) {
+          toast.error(message)
+      }
+
+      if(isSuccess || user ) {
+          navigate('/')
+      } 
+
+      dispatch (reset())
+
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
+
+    const onChange = (e) => {
+        setFromData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }))
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        const userData = {
+          email,
+          password
+        }
+
+        dispatch(login(userData))
+    }
+
+
+    if(isLoading){
+      return <Spinner />
+    }
+
+    return (
+     <>
+      <section className='heading'>
         <h1>
-          <FaSignInAlt /> Login
+          <FaSignInAlt /> Register
         </h1>
-        <p> Login and start getting recommendations</p>
+        <p>Login to start the application</p>
       </section>
 
-      <section className="form">
+      <section className='form'>
         <form onSubmit={onSubmit}>
-         
-          <div className="form-group">
+        
+          
+          <div className='form-group'>
             <input
-              type="email"
-              className="form-control"
-              id="email"
-              name="email"
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
               value={email}
-              placeholder="Enter your email"
+              placeholder='Enter your email'
               onChange={onChange}
             />
           </div>
-          <div className="form-group">
+          <div className='form-group'>
             <input
-              type="password"
-              className="form-control"
-              id="password"
-              name="password"
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
               value={password}
-              placeholder="Enter password"
+              placeholder='Enter password'
               onChange={onChange}
             />
           </div>
-
-          <div className="form-group">
-            <button type="submit" className='btn btn-block'>Submit</button>
-            </div>
+        
+          <div className='form-group'>
+            <button type='submit' className='btn btn-block'>
+              Submit
+            </button>
+          </div>
         </form>
       </section>
     </>
-  );
+    )
+  
 }
 
-export default Login;
+export default Login
+
